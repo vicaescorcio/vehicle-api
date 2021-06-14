24f6072696dd606ee86149253a3ad80797c09a44
Vicente Lucas Escorcio <vica.escorcio@gmail.com>
2021-06-14 10:31:58 -0300

    Set build file correctly to serve react client

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ require('dotenv').config({ path: '.env' })
 const path = require('path');
 const express = require('express');
 const app = require('./src/controllers/app');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // At the NODE server
-app.use(express.static(path.resolve(__dirname, '../vehicle-react-client/build')));
+app.use(express.static(path.resolve(__dirname, './vehicle-react-client/build')));
 
 // All gets, except /api, will be handle by react app
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../vehicle-react-client/build', 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './vehicle-react-client/build', 'index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
