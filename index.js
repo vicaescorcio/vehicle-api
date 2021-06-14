a02b73d655f95834fb45af15d3c2a5d87c44bb8d
Vicente Lucas Escorcio <vica.escorcio@gmail.com>
2021-06-13 21:04:37 -0300

    Adds deploy scripts

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 require('dotenv').config({ path: '.env' })
 
-const app = require("./src/controllers/app");
+const path = require('path');
+const express = require('express');
+const app = require('./src/controllers/app');
 const port = 3000;
+
+// At the NODE server
+app.use(express.static(path.resolve(__dirname, '../vehicle-react-client/build')));
+
+// All gets, except /api, will be handle by react app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../vehicle-react-client/build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
