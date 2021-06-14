e52e08996a809e270a858abdac8b55c280b76b94
Vicente Lucas Escorcio <vica.escorcio@gmail.com>
2021-06-14 18:46:17 -0300

    Fix data pulling uniqueness error

--- src/seeders/20210612173551-pull-vehicles.js
+++ src/seeders/20210612173551-pull-vehicles.js
@@ -1,38 +1,40 @@
-'use strict';
+"use strict";
 
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    console.log("Pulling vehicles from API...");
 
-    console.log('Pulling vehicles from API...');
+    const res = await axios.get(
+      "http://fipeapi.appspot.com/api/1/carros/veiculos/39.json"
+    );
+    const vehicles = res.data;
 
-    const res = await axios.get('http://fipeapi.appspot.com/api/1/carros/veiculos/39.json')
-    const vehicles = res.data
-
-    console.log('done!');
+    console.log("done!");
 
     const pulledVehicles = vehicles.map((vehicle, index) => {
       return {
-        name: vehicle['name'],
-        externalId: vehicle['id'],
-        externalKey: vehicle['key'],
+        name: vehicle["name"],
+        externalId: vehicle["id"],
+        externalKey: vehicle["key"],
         makeId: 39,
         createdAt: Date().toISOString,
         updatedAt: Date().toISOString,
-      }
+      };
     });
 
-    console.log(pulledVehicles);
+    console.log("Inserting new data...");
 
-    console.log('Inserting and uploading in databases...');
+    await queryInterface.bulkInsert("Vehicles", pulledVehicles, {
+      fields: ["externalKey"],
+      ignoreDuplicates: true,
+    });
 
-    await queryInterface.bulkInsert('Vehicles', pulledVehicles);
-
-    console.log('done!');
+    console.log("done!");
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Vehicles', null, {});
-  }
+    await queryInterface.bulkDelete("Vehicles", null, {});
+  },
 };
